9f6b366c13c009af69429a53e6eb56f70518e812
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-11-03 17:32:10 -0500

    Agregar archivos de proyecto.

---
+++ DependencyServicesSample/DependencyServicesSample.Android/BatteryImplementation.cs
@@ -0,0 +1,135 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyServicesSample.Droid
+{
+    public class BatteryImplementation : IBattery
+    {
+        public BatteryImplementation()
+        {
+        }
+
+        public int RemainingChargePercent
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            var level = battery.GetIntExtra(BatteryManager.ExtraLevel, -1);
+                            var scale = battery.GetIntExtra(BatteryManager.ExtraScale, -1);
+
+                            return (int)Math.Floor(level * 100D / scale);
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public DependencyServicesSample.BatteryStatus Status
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+                            if (isCharging)
+                                return DependencyServicesSample.BatteryStatus.Charging;
+
+                            switch (status)
+                            {
+                                case (int)BatteryStatus.Charging:
+                                    return DependencyServicesSample.BatteryStatus.Charging;
+                                case (int)BatteryStatus.Discharging:
+                                    return DependencyServicesSample.BatteryStatus.Discharging;
+                                case (int)BatteryStatus.Full:
+                                    return DependencyServicesSample.BatteryStatus.Full;
+                                case (int)BatteryStatus.NotCharging:
+                                    return DependencyServicesSample.BatteryStatus.NotCharging;
+                                default:
+                                    return DependencyServicesSample.BatteryStatus.Unknown;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+
+        public PowerSource PowerSource
+        {
+            get
+            {
+                try
+                {
+                    using (var filter = new IntentFilter(Intent.ActionBatteryChanged))
+                    {
+                        using (var battery = Application.Context.RegisterReceiver(null, filter))
+                        {
+                            int status = battery.GetIntExtra(BatteryManager.ExtraStatus, -1);
+                            var isCharging = status == (int)BatteryStatus.Charging || status == (int)BatteryStatus.Full;
+
+                            var chargePlug = battery.GetIntExtra(BatteryManager.ExtraPlugged, -1);
+                            var usbCharge = chargePlug == (int)BatteryPlugged.Usb;
+                            var acCharge = chargePlug == (int)BatteryPlugged.Ac;
+
+                            bool wirelessCharge = false;
+                            wirelessCharge = chargePlug == (int)BatteryPlugged.Wireless;
+
+                            isCharging = (usbCharge || acCharge || wirelessCharge);
+
+                            if (!isCharging)
+                                return DependencyServicesSample.PowerSource.Battery;
+                            else if (usbCharge)
+                                return DependencyServicesSample.PowerSource.Usb;
+                            else if (acCharge)
+                                return DependencyServicesSample.PowerSource.Ac;
+                            else if (wirelessCharge)
+                                return DependencyServicesSample.PowerSource.Wireless;
+                            else
+                                return DependencyServicesSample.PowerSource.Other;
+                        }
+                    }
+                }
+                catch
+                {
+                    System.Diagnostics.Debug.WriteLine("Ensure you have android.permission.BATTERY_STATS");
+                    throw;
+                }
+            }
+        }
+    }
+}
